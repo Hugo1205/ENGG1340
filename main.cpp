f343ccfd39c7b23f247184c7ab239225d98457ed
wasiflatifhussain <md.wasiflatif9859@gmail.com>
2022-04-05 14:31:55 +0800

    Menu Foundation
    Making the basic menu layout for the game.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
-#include "io.h"
-#include "game_type.h"
-void menu(){
-    
+//#include "io.h"
+//#include "game_type.h"
+using namespace std;
+
+void menu ();
+
+int main () {
+    menu ();
+    return 0;
 }
-int main(){
-    return 1;
+
+void menu () {
+  int option;
+  cout<<"Welcome to Tetris!"<<endl;
+  cout<<"Select Option: "<<endl;
+  cout<<"1. Start New Game"<<endl;
+  cout<<"2. Last Game Status"<<endl;
+  cout<<"3. Game Rules"<<endl;
+  cout<<"Enter Option Number: ";
+  cin>>option;
 }
