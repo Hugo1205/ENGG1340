39d3a375eb0c1adf15f900388796259cb8bd45fd
Hugo1205 <ss22hugo@connect.hku.hk>
2022-05-02 01:52:17 +0800

    fixed error control

--- source/main.cpp
+++ source/main.cpp
@@ -96,34 +96,21 @@ void newGame () {
 //          the result.
 
 void loadGame () {
-  games game;
+  Games game;
   string fname;
   cout << "Input load file name:";
   cin >> fname;
-  string temp = fname;
-  ifstream fin;
-  if(temp.find(".txt")==-1)//if the user do no add .txt we add it for them
-    temp += ".txt";
-  if(temp.find("./saves/")==-1)
-    temp = "./saves/" + temp;
-  fin.open(temp.c_str());
-  while (fin.fail()) {
-      cout<<"Enter an existing file name: ";
-      cin>>fname;
-      string temp = fname;
-      if(temp.find(".txt")==-1)//if the user do no add .txt we add it for them
-        temp += ".txt";
-      if(temp.find("./saves/")==-1)
-        temp = "./saves/" + temp;
-      fin.open(temp.c_str());
-  }
-  //cout<<"outside while"<<fname<<endl;
-  fin.close();
   //cout<<"after close"<<fname<<endl;
   if(fname.find(".txt")==-1)//if the user do no add .txt we add it for them
     fname += ".txt";
-  ReadGameFromFile(game,fname);
-  if(game_main(game)){
+  while (ReadGameFromFile(game,fname)){
+    cout << "Error. Please Enter a valid file name:";
+    cin >> fname;
+    //cout<<"after close"<<fname<<endl;
+    if(fname.find(".txt")==-1)//if the user do no add .txt we add it for them
+      fname += ".txt";
+  }
+  if(GameMain(game)){
     cout << "Input save file name:";
     cin >> fname;
     if(fname.find(".txt")==-1)//if the user do no add .txt we add it for them
